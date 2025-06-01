6e4a1f4bc892953089b72a9a1393e0c53ba7d018
venuasarelly <asarellyvenusiri1@gmail.com>
2025-06-01 15:09:08 +0530

    added

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,6 +2,6 @@
 exports.lambdaHandler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambdsssa!" }),
+    body: JSON.stringify({ message: "Hello from Lambdsssa! venu verery" }),
   };
 };
